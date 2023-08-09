ce4c6c731a0d6223f287468a14ec2b4c34b23f45
EmptyVoid <VoidEmpty@users.noreply.github.com>
2023-08-09 17:35:26 +0300

    Remove unused configs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+//! This example circulary lights the LEDs on the board.
 #![no_main]
 #![no_std]
 
@@ -6,55 +7,60 @@ use panic_halt as _;
 use stm32f407g_disc as board;
 
 use crate::board::{
-    hal::prelude::*,
     hal::stm32,
+    hal::{delay::Delay, prelude::*},
     led::{LedColor, Leds},
 };
+
+use cortex_m::peripheral::Peripherals;
 
 use cortex_m_rt::entry;
 
 #[entry]
 fn main() -> ! {
-    if let Some(p) = stm32::Peripherals::take() {
+    if let (Some(p), Some(cp)) = (stm32::Peripherals::take(), Peripherals::take()) {
         let gpiod = p.GPIOD.split();
 
         // Initialize on-board LEDs
         let mut leds = Leds::new(gpiod);
 
-        // Endlessly blink the 4 LEDs in a circle, delaying by executing the state write many times
-        // in a row
+        // Constrain clock registers
+        let rcc = p.RCC.constrain();
+
+        // Configure clock to 168 MHz (i.e. the maximum) and freeze it
+        let clocks = rcc.cfgr.sysclk(168.mhz()).freeze();
+
+        // Get delay provider
+        let mut delay = Delay::new(cp.SYST, clocks);
+
         loop {
-            for _ in 0..10_000 {
-                leds[LedColor::Orange].on();
-            }
+            // Turn LEDs on one after the other with 500ms delay between them
+            leds[LedColor::Orange].on();
+            delay.delay_ms(500_u16);
+            leds[LedColor::Red].on();
+            delay.delay_ms(500_u16);
+            leds[LedColor::Blue].on();
+            delay.delay_ms(500_u16);
+            leds[LedColor::Green].on();
+            delay.delay_ms(500_u16);
 
-            for _ in 0..10_000 {
-                leds[LedColor::Orange].off();
-            }
+            // Delay twice for half a second due to limited timer resolution
+            delay.delay_ms(500_u16);
+            delay.delay_ms(500_u16);
 
-            for _ in 0..10_000 {
-                leds[LedColor::Red].on();
-            }
+            // Turn LEDs off one after the other with 500ms delay between them
+            leds[LedColor::Orange].off();
+            delay.delay_ms(500_u16);
+            leds[LedColor::Red].off();
+            delay.delay_ms(500_u16);
+            leds[LedColor::Blue].off();
+            delay.delay_ms(500_u16);
+            leds[LedColor::Green].off();
+            delay.delay_ms(500_u16);
 
-            for _ in 0..10_000 {
-                leds[LedColor::Red].off();
-            }
-
-            for _ in 0..10_000 {
-                leds[LedColor::Blue].on();
-            }
-
-            for _ in 0..10_000 {
-                leds[LedColor::Blue].off();
-            }
-
-            for _ in 0..10_000 {
-                leds[LedColor::Green].on();
-            }
-
-            for _ in 0..10_000 {
-                leds[LedColor::Green].off();
-            }
+            // Delay twice for half a second due to limited timer resolution
+            delay.delay_ms(500_u16);
+            delay.delay_ms(500_u16);
         }
     }
 
